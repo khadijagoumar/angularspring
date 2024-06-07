72ed5346ef9b5ce6a3ccf212d8c6cd4f00de8fd4
khadija_goumar <khadijagoumar11@gmail.com>
2024-06-07 06:48:56 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,31 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ClientHomepageComponent } from './client-homepage/client-homepage.component';
+import {RouterLink, RouterOutlet} from "@angular/router";
+import {AppRoutingModule} from "./app-routing.module";
+import { DynamicFormComponent } from './dynamic-form/dynamic-form.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+import {FactureFormComponent} from "./facture-form/facture-form.component";
+import { ConfirmSignatureComponent } from './confirm-signature/confirm-signature.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientHomepageComponent,
+    DynamicFormComponent,
+    FactureFormComponent,
+    ConfirmSignatureComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterLink,
+    HttpClientModule,
+    RouterOutlet,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
